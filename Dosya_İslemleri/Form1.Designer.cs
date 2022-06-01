c1f354766b0cc2d15d9a3aa029d84b8df6036278
bayhickimse173@gmail.com <bayhickimse173@gmail.com>
2022-06-01 16:16:33 +0300

    Add project files.

---
+++ Dosya_İslemleri/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace Dosya_İslemleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonOlustur = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonYolSec = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonTemizle = new System.Windows.Forms.Button();
+            this.buttonOku = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.buttonTemizle2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(144, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(144, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dosya Adı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Dosya Yolu :";
+            // 
+            // buttonOlustur
+            // 
+            this.buttonOlustur.Location = new System.Drawing.Point(153, 130);
+            this.buttonOlustur.Name = "buttonOlustur";
+            this.buttonOlustur.Size = new System.Drawing.Size(75, 36);
+            this.buttonOlustur.TabIndex = 4;
+            this.buttonOlustur.Text = "Oluştur";
+            this.buttonOlustur.UseVisualStyleBackColor = true;
+            this.buttonOlustur.Click += new System.EventHandler(this.buttonOlustur_Click);
+            // 
+            // buttonYolSec
+            // 
+            this.buttonYolSec.Location = new System.Drawing.Point(53, 130);
+            this.buttonYolSec.Name = "buttonYolSec";
+            this.buttonYolSec.Size = new System.Drawing.Size(75, 36);
+            this.buttonYolSec.TabIndex = 5;
+            this.buttonYolSec.Text = "Yol Seç";
+            this.buttonYolSec.UseVisualStyleBackColor = true;
+            this.buttonYolSec.Click += new System.EventHandler(this.buttonYolSec_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.buttonYolSec);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.buttonOlustur);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(294, 216);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dosya Oluştur ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonTemizle);
+            this.groupBox2.Controls.Add(this.buttonOku);
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(313, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(352, 215);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Dosya Oku";
+            // 
+            // buttonTemizle
+            // 
+            this.buttonTemizle.Location = new System.Drawing.Point(187, 169);
+            this.buttonTemizle.Name = "buttonTemizle";
+            this.buttonTemizle.Size = new System.Drawing.Size(88, 40);
+            this.buttonTemizle.TabIndex = 2;
+            this.buttonTemizle.Text = "Temizle";
+            this.buttonTemizle.UseVisualStyleBackColor = true;
+            this.buttonTemizle.Click += new System.EventHandler(this.buttonTemizle_Click);
+            // 
+            // buttonOku
+            // 
+            this.buttonOku.Location = new System.Drawing.Point(91, 169);
+            this.buttonOku.Name = "buttonOku";
+            this.buttonOku.Size = new System.Drawing.Size(90, 40);
+            this.buttonOku.TabIndex = 1;
+            this.buttonOku.Text = "Oku";
+            this.buttonOku.UseVisualStyleBackColor = true;
+            this.buttonOku.Click += new System.EventHandler(this.buttonOku_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(6, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(337, 144);
+            this.listBox1.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.buttonTemizle2);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.richTextBox1);
+            this.groupBox3.Location = new System.Drawing.Point(671, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(363, 215);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Metin Kaydet";
+            // 
+            // buttonTemizle2
+            // 
+            this.buttonTemizle2.Location = new System.Drawing.Point(183, 169);
+            this.buttonTemizle2.Name = "buttonTemizle2";
+            this.buttonTemizle2.Size = new System.Drawing.Size(88, 40);
+            this.buttonTemizle2.TabIndex = 9;
+            this.buttonTemizle2.Text = "Temizle";
+            this.buttonTemizle2.UseVisualStyleBackColor = true;
+            this.buttonTemizle2.Click += new System.EventHandler(this.buttonTemizle2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 19);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(351, 144);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1046, 241);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonOlustur;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonYolSec;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonOku;
+        private System.Windows.Forms.Button buttonTemizle;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button buttonTemizle2;
+    }
+}
+
---
+++ Dosya_İslemleri/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Dosya_İslemleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string sDosyaAdi, sDosyaYolu;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonYolSec_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                sDosyaYolu = folderBrowserDialog1.SelectedPath.ToString();
+                textBox2.Text = sDosyaYolu;
+            }
+        }
+
+        private void buttonOku_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog1.FileName);
+                for (string sSatir=sr.ReadLine() ; sSatir != null;sSatir=sr.ReadLine())
+                {
+                    listBox1.Items.Add(sSatir);
+                }
+                sr.Close();
+            }
+        }
+
+        private void buttonTemizle_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void buttonKaydet_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter="Metin Dosyaları|*.txt";
+            saveFileDialog1.Title = "Metni Kaydet";
+            saveFileDialog1.ShowDialog();
+            StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+            sw.WriteLine(richTextBox1.Text);
+            sw.Close();
+        }
+
+        private void buttonTemizle2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void buttonOlustur_Click(object sender, EventArgs e)
+        {
+            sDosyaAdi = textBox1.Text;
+            StreamWriter sw = File.CreateText(sDosyaYolu + "\\" + sDosyaAdi + ".txt");
+            sw.Close();
+        }
+    }
+}
